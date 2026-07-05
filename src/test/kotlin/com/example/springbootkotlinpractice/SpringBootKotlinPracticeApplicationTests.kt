70505253a08f686cf9f20511d0afd56c007fa2f5
한규호 <hkh7670@gmail.com>
2026-07-06 03:12:36 +0900

    logback 수정

--- src/test/kotlin/com/example/springbootkotlinpractice/SpringBootKotlinPracticeApplicationTests.kt
+++ src/test/kotlin/com/example/springbootkotlinpractice/SpringBootKotlinPracticeApplicationTests.kt
@@ -2,8 +2,10 @@ package com.example.springbootkotlinpractice
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootKotlinPracticeApplicationTests {
 
     @Test
